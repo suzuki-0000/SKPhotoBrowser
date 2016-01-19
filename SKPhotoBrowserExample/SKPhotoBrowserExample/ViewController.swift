ab2191aab8df7979678af4fe17fb38d83da754eb
keishi suzuki <keishi.1983@gmail.com>
2016-01-19 13:58:32 +0900

    update

--- SKPhotoBrowserExample/SKPhotoBrowserExample/ViewController.swift
+++ SKPhotoBrowserExample/SKPhotoBrowserExample/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     var images = [SKPhoto]()
     var caption = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                   "It has survived not on/Users/a12556/workspace/_origin/SKPhotoBrowser/README.mdly five centuries, but also the leap into electronic typesetting",
+                   "It has survived not only five centuries, but also the leap into electronic typesetting",
                    "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
